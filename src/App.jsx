85c798d7a758405ad36625e1bcd8985a71bcb2aa
Santiago Cendales <srcendales94@gmail.com>
2023-04-20 12:37:26 -0500

    Add functional search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,69 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
 import { MovieItem, NoResult } from './components/MovieItem';
 import { useMovies } from './hooks/useMovies';
 
 const api_key = '2b4af9fd'
 
+const useSearch = () => {
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if(isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return
+    }
+    if(search === '') {
+      setError('No se puede buscar la película')
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar un numero')
+      return;
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe contener 3 caracteres')
+      return;
+    }
+
+    setError(null);
+  }, [search])
+
+  return {
+    search,
+    setSearch,
+    error
+  }
+}
+
 function App() {
+
+  const { search, error, setSearch } = useSearch()
 
   const { mappedMovies, hasMovies } = useMovies();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search)
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+  
   return (
     <div className='page'>
       <header>
         <h1>Movie Search</h1>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <input
+            name='search'
+            value={search}
+            onChange={handleChange}
             type="text"
             placeholder='Iron man, Captain America, ...'
           />
@@ -21,6 +71,7 @@ function App() {
             Search
           </button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
